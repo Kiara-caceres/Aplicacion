e9c5d2145a84be4e41041ab126aa163cddbc58bc
Kiara Caceres <dukehida@hotmail.com>
2024-09-07 11:32:53 -0500

    create script

---
+++ age_group.py
@@ -0,0 +1,32 @@
+def get_age_group(age):
+ 
+     """
+     Devuelve el grupo de edad basado en la edad en años dentro del intervalo 0..150
+     de lo contrario, devuelve 'desconocido'.
+     """
+ 
+     if 0 <= age <= 14:
+         return 'infancia'
+         # Completa esta función para que pase la prueba unitaria
+     if 25 <= age < 65:
+         return 'adulto'
+     if 14 < age <25:
+         return 'juventud'
+     if 65 <= age <= 80:
+         return 'vejez'
+     return 'desconocido'
+ 
+def test_get_age_group():
+ 
+     """prueba unitaria para get_age_group"""
+ 
+     assert get_age_group(-1) == 'desconocido'
+     assert get_age_group(0) == 'infancia'
+     assert get_age_group(14) == 'infancia'
+     assert get_age_group(15) == 'juventud'
+     assert get_age_group(24) == 'juventud'
+     assert get_age_group(25) == 'adulto'
+     assert get_age_group(64) == 'adulto'
+     assert get_age_group(65) == 'vejez'
+     assert get_age_group(80) == 'vejez'
+     assert get_age_group(150) == 'desconocido'
